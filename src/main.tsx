5645211917b5c1973092160c83a1481da8243796
migueldcdev <migueldc.dev@protonmail.com>
2025-03-22 12:31:09 +0000

    add: prettier organize imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
+import { Provider } from '@/components/ui/provider';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from '@/components/ui/provider';
-import './index.css';
 import App from './App.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
