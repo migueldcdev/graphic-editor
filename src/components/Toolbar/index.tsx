997bf0f06c62627801a11f23d69dfd4252b1e192
migueldcdev <migueldc.dev@protonmail.com>
2025-03-22 16:36:19 +0000

    done: toolbar

--- src/components/Toolbar/index.tsx
+++ src/components/Toolbar/index.tsx
@@ -1,9 +1,19 @@
-import { Box } from '@chakra-ui/react';
+import { Tooltip } from '@/components/ui/tooltip';
+import { Box, IconButton } from '@chakra-ui/react';
+import { LuRectangleHorizontal } from 'react-icons/lu';
 
 export const Toolbar = () => {
   return (
-    <Box height="100vh" width="1/12" bgColor="gray.300">
-      Hello
+    <Box height="100vh" paddingLeft="3" paddingTop="2" width="12">
+      <Tooltip
+        content="Select area in manuscript"
+        openDelay={250}
+        closeDelay={100}
+      >
+        <IconButton aria-label="Select " variant="surface" size="sm">
+          <LuRectangleHorizontal />
+        </IconButton>
+      </Tooltip>
     </Box>
   );
 };
