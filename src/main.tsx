7a838485c391b9afd77e14644e22f48f00bcb7bb
migueldcdev <migueldc.dev@protonmail.com>
2025-03-22 16:33:09 +0000

    style: force light theme

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider>
+    <Provider forcedTheme="light">
       <App />
     </Provider>
   </StrictMode>,
