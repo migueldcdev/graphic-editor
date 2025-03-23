6a7347d7762265288561d373c571539d905960df
migueldcdev <migueldc.dev@protonmail.com>
2025-03-23 15:42:02 +0000

    done: create rectangle action

--- src/features/canvas/canvasSlice.ts
+++ src/features/canvas/canvasSlice.ts
@@ -1,11 +1,33 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { v4 as uuidv4 } from "uuid";
+
+type Coordinates = {
+  x: number;
+  y: number;
+}
+
+export type Rectangle = {
+  id: string;
+  coordinates: Coordinates
+  width: number;
+  height: number;
+};
 
 type CanvasState = {
-  isRectangleSelected: boolean;
+  isCreateRectangleSelected: boolean;
+  rectangles: Rectangle[];
 };
 
 const initialState: CanvasState = {
-  isRectangleSelected: false,
+  isCreateRectangleSelected: false,
+  rectangles: [
+    {
+      id: 'string',
+      coordinates: {x: 10, y: 4},
+      width: 100,
+      height: 20,
+    },
+  ],
 };
 
 const canvasSlice = createSlice({
@@ -13,11 +35,20 @@ const canvasSlice = createSlice({
   initialState,
   reducers: {
     selectRectangle: (state) => {
-      state.isRectangleSelected = true;
+      state.isCreateRectangleSelected = true;
     },
     unselectRectangle: (state) => {
-      state.isRectangleSelected = false;
+      state.isCreateRectangleSelected = false;
     },
+    addRectangle: (state, action: PayloadAction<Coordinates>) => {
+      const rectangle = {
+        id: uuidv4(),
+        coordinates: action.payload,
+        width: 0,
+        height: 0,
+      }
+      state.rectangles.push(rectangle)
+    }
   },
 });
 
